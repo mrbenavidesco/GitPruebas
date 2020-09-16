8cce6fc8e4353ddfbc9c30139106f1ee0b2a8f44
TFlowDesa <tflowdesa@gmail.com>
2020-09-16 15:05:28 -0600

    Primer version del Script Shell

---
+++ script.sh
@@ -0,0 +1,15 @@
+for idx in 1 2 3 4 5 6 7 8 9 10
+do
+	echo "Contador: $idx";
+done;
+
+for idx in $@
+do 
+	let cont = $cont + 1;
+	echo "Parametro $cont : $idx";
+done;
+
+echo "Usuario: $USER";
+echo "Nombre : $USERNAME";
+echo "Host   : $HOSTNAME";
+echo "Tiempo : $SECONDS";
